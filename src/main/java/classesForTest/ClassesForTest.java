fe711290b247d5e563e623cc0ed61bb46b2f9485
Nargiza Denizov <nariden90@gmail.com>
2019-01-12 19:31:38 -0600

    my second try Nargiza

--- src/main/java/classesForTest/ClassesForTest.java
+++ src/main/java/classesForTest/ClassesForTest.java
@@ -7,4 +7,13 @@ public class ClassesForTest {
     int result =a+b;
         return result;
     }
+
+    public static void main(String[] args) {
+        String a = "Hello you guys! I'm glad that we are doing git! Nargiza";
+        StringBuilder b= new StringBuilder(a);
+        System.out.println(b.reverse().toString());
+    }
+
+
+
 }
