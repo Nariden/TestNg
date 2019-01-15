f7d8a6170e62040250678f8b7ca4842c194d46ce
Nargiza Denizov <nariden90@gmail.com>
2019-01-15 16:19:42 -0600

    Today is a good day! I will miss these days!

--- src/main/java/classesForTest/ClassesForTest.java
+++ src/main/java/classesForTest/ClassesForTest.java
@@ -12,8 +12,27 @@ public class ClassesForTest {
         String a = "Hello you guys! I'm glad that we are doing git! Nargiza";
         StringBuilder b= new StringBuilder(a);
         System.out.println(b.reverse().toString());
+
+
+        System.out.println(reversedStr("Hey,team! Today is a good day!"));
+
     }
 
 
+
+
+
+
+    public static String reversedStr(String str){
+        String reverse="";
+        for(int i =str.length()-1; i >=0; i --){
+
+            reverse+=str.charAt(i);
+
+        }
+
+
+        return reverse;
+    }
 
 }
