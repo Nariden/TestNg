5ad8562006089753835268d2e41bd4c83be1adea
Nargiza Denizov <nariden90@gmail.com>
2019-01-17 09:52:43 -0600

    Testing from Intelije bar commands

--- src/test/java/ClassesForTestTest.java
+++ src/test/java/ClassesForTestTest.java
@@ -7,16 +7,16 @@ public class ClassesForTestTest {
 
     @Test
     public void testSumOfNum() {
-        SoftAssert sa=new SoftAssert();
-ClassesForTest tr=new ClassesForTest();
-int sum= tr.sumOfNum(3,6);
+        SoftAssert sa = new SoftAssert();
+        ClassesForTest tr = new ClassesForTest();
+        int sum = tr.sumOfNum(3, 6);
         System.out.println("Fail one");
-        sa.assertEquals(sum,6);
+        sa.assertEquals(sum, 6);
         System.out.println("Pass one");
-        sa.assertEquals(sum,9);
+        sa.assertEquals(sum, 9);
         sa.assertAll();
 
 
-        Assert.assertEquals(4,4);//Nargiza, Jan 15
+        Assert.assertEquals(4, 4);//Nargiza, Jan 15
     }
 }
